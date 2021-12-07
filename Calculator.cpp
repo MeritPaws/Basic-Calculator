8f23d55184442c70af13c35525115d6218eac93a
MeritPaws <devinmandurrago@gmail.com>
2021-12-06 19:20:16 -0800

    Added spaces for better readability of code.

--- Calculator.cpp
+++ Calculator.cpp
@@ -9,12 +9,14 @@ int main()
     double secondvalue;
     string operand;
     double total;
+
     cout << "Please enter your first value:" << endl;
     cin >> firstvalue;
     cout << "Please enter an operand following " << firstvalue << endl;
     cin >> operand;
     cout << "Please enter a second value:" << endl;
     cin >> secondvalue;
+
     if(operand == "+")
     {
         total = (firstvalue + secondvalue);
@@ -31,7 +33,10 @@ int main()
     {
         total = (firstvalue * secondvalue);
     }
+    
     cout << "The answer is " << total << endl;
+
     return 0;
+
 }
 
